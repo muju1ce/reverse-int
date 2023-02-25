739f9a72f820fd9965630ba5c01bd0b296f3565c
Art Jane <muju1ce@icloud.com>
2023-02-25 17:41:43 +0600

    feat: fix file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    if ( n === Math.abs(n)) {
+        return reverse(n);
+    } else if ( n !== Math.abs(n)) {
+        return Math.abs(reverse(n))
+    }
 }
